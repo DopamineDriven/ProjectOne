a18f8c80d6bb6f7b45cb9b527805cbd7029045f4
lmalexander <elizabeth.alexander@northwestern.edu>
2019-12-07 13:33:22 -0600

    console logged response object

--- scripts.js
+++ scripts.js
@@ -1,17 +1,31 @@
 console.log("Hello!");
 
-// return user birthday
+//submit button click event
+
+    // return user input birthday
 $("#submitBtn").on("click", function(event) {
     event.preventDefault();
     let birthdayInput = $("#birthdayInput").val();
     console.log(birthdayInput);
 
+    // create birthday month and day variables to append to queryURL
     let birthdayMonth = moment(birthdayInput, "MM-DD-YY").format("MM");
     let birthdayDay = moment(birthdayInput, "MM-DD-YY").format("DD");
-
-// append user birthday to Wikipedia queryURL search 
+    
+    // append variables to Wikipedia queryURL search 
  var queryURL = "https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/" + birthdayMonth + "/" + birthdayDay;
 
+    // check queryURL
  console.log(queryURL);
-})
-   
+
+    // return response object
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
+});
+
+});
+
+
