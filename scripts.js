d1b7401d58601389c31fd77c16b083b0d09e2a2a
lmalexander <elizabeth.alexander@northwestern.edu>
2019-12-07 12:53:32 -0600

    index.html birthday search box and scripts.js queryUrl

---
+++ scripts.js
@@ -0,0 +1,17 @@
+console.log("Hello!");
+
+// return user birthday
+$("#submitBtn").on("click", function(event) {
+    event.preventDefault();
+    let birthdayInput = $("#birthdayInput").val();
+    console.log(birthdayInput);
+
+    let birthdayMonth = moment(birthdayInput, "MM-DD-YY").format("MM");
+    let birthdayDay = moment(birthdayInput, "MM-DD-YY").format("DD");
+
+// append user birthday to Wikipedia queryURL search 
+ var queryURL = "https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/" + birthdayMonth + "/" + birthdayDay;
+
+ console.log(queryURL);
+})
+  
