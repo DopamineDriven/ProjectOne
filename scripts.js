378663ee41462e8fe5ed66a020d39ffc12d55487
lmalexander <elizabeth.alexander@northwestern.edu>
2019-12-07 18:40:57 -0600

    console log cardText and cardYear

--- scripts.js
+++ scripts.js
@@ -2,44 +2,49 @@ console.log("Hello!");
 
 //submit button click event
 
-    // return user input birthday
+// return user input birthday
 $("#submitBtn").on("click", function(event) {
     event.preventDefault();
     let birthdayInput = $("#birthdayInput").val();
     console.log(birthdayInput);
 
-    // create birthday month and day variables to append to queryURL
+// create birthday month and day variables to append to queryURL
     let birthdayMonth = moment(birthdayInput, "MM-DD-YY").format("MM");
     let birthdayDay = moment(birthdayInput, "MM-DD-YY").format("DD");
     
-    // append variables to Wikipedia queryURL search 
+// append variables to Wikipedia queryURL search 
  var queryURL = "https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/" + birthdayMonth + "/" + birthdayDay;
 
-    // check queryURL
+// check queryURL
  console.log(queryURL);
 
-    // return response object
+// return response object
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response) {
     console.log(response); 
-    // access response object data
-for (let index = 0; index < response.length; index++) {
-    // card text
-let cardText = response.events[i].text;
-console.log(cardText);
+    
+// access response object data
+for (let index = 0; index < response.events.length; index++) {
+    
+// card text
+    let cardText = response.events[index].text;
+    console.log(cardText);
+// card year
+    let cardYear = response.events[index].year;
+    console.log(cardYear);
+// card thumbnail
+    let cardThumbnail = response.events[index].pages.thumbnail.source.val();
+    console.log(cardThumbnail); 
+
+// append data to html cards
+
 }
 
         
 
-    // card year
-
-    // card thumbnail 
-
-
-
-    // append data to html cards
+    
 
 }); // end ajax call
 
